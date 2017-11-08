2f20b9e69df1997ea15e9e5dc353753a20a37927
lance <carck@foxmail.com>
2017-11-08 12:34:20 +0800

    REVERT: 300HZ kernel.  not ready

--- drivers/soc/qcom/smp2p_test.c
+++ drivers/soc/qcom/smp2p_test.c
@@ -86,7 +86,7 @@ static void smp2p_ut_local_basic(struct seq_file *s)
 		/* verify port was opened */
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_data.cb_completion, msecs_to_jiffies(500)), >, 0);
+					&cb_data.cb_completion, HZ / 2), >, 0);
 		UT_ASSERT_INT(cb_data.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_data.event_open, ==, 1);
 		UT_ASSERT_INT(rmp->rx_interrupt_count, ==, 2);
@@ -174,7 +174,7 @@ static void smp2p_ut_local_late_open(struct seq_file *s)
 		/* verify port was opened */
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_data.cb_completion, msecs_to_jiffies(500)),
+					&cb_data.cb_completion, HZ / 2),
 			>, 0);
 		UT_ASSERT_INT(cb_data.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_data.event_open, ==, 1);
@@ -266,7 +266,7 @@ static void smp2p_ut_local_early_open(struct seq_file *s)
 
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_data.cb_completion, msecs_to_jiffies(125)),
+					&cb_data.cb_completion, HZ / 8),
 			==, 0);
 		UT_ASSERT_INT(cb_data.cb_count, ==, 0);
 		UT_ASSERT_INT(cb_data.event_open, ==, 0);
@@ -295,7 +295,7 @@ static void smp2p_ut_local_early_open(struct seq_file *s)
 
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_data.cb_completion, msecs_to_jiffies(500)),
+					&cb_data.cb_completion, HZ / 2),
 			>, 0);
 		UT_ASSERT_INT(cb_data.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_data.event_open, ==, 1);
@@ -385,7 +385,7 @@ static void smp2p_ut_mock_loopback(struct seq_file *s)
 		local = msm_smp2p_init_rmt_lpb_proc(SMP2P_REMOTE_MOCK_PROC);
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&rmp->cb_completion, msecs_to_jiffies(500)),
+					&rmp->cb_completion, HZ / 2),
 			>, 0);
 		UT_ASSERT_INT(rmp->rx_interrupt_count, ==, 2);
 
@@ -399,7 +399,7 @@ static void smp2p_ut_mock_loopback(struct seq_file *s)
 		rmp->tx_interrupt();
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&rmp->cb_completion, msecs_to_jiffies(500)),
+					&rmp->cb_completion, HZ / 2),
 			>, 0);
 
 		/* Verify Echo Response */
@@ -422,7 +422,7 @@ static void smp2p_ut_mock_loopback(struct seq_file *s)
 		rmp->tx_interrupt();
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&rmp->cb_completion, msecs_to_jiffies(500)),
+					&rmp->cb_completion, HZ / 2),
 			>, 0);
 
 		/* Verify PINGPONG Response */
@@ -444,7 +444,7 @@ static void smp2p_ut_mock_loopback(struct seq_file *s)
 		rmp->tx_interrupt();
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&rmp->cb_completion, msecs_to_jiffies(500)),
+					&rmp->cb_completion, HZ / 2),
 			>, 0);
 
 		/* Verify CLEARALL response */
@@ -495,7 +495,7 @@ static void smp2p_ut_remote_inout_core(struct seq_file *s, int remote_pid)
 		UT_ASSERT_INT(ret, ==, 0);
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_out.cb_completion, msecs_to_jiffies(500)),
+					&cb_out.cb_completion, HZ / 2),
 			>, 0);
 		UT_ASSERT_INT(cb_out.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_out.event_open, ==, 1);
@@ -506,7 +506,7 @@ static void smp2p_ut_remote_inout_core(struct seq_file *s, int remote_pid)
 		UT_ASSERT_INT(ret, ==, 0);
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_in.cb_completion, msecs_to_jiffies(500)),
+					&cb_in.cb_completion, HZ / 2),
 			>, 0);
 		UT_ASSERT_INT(cb_in.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_in.event_open, ==, 1);
@@ -524,7 +524,7 @@ static void smp2p_ut_remote_inout_core(struct seq_file *s, int remote_pid)
 		/* Verify inbound reply */
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_in.cb_completion, msecs_to_jiffies(500)),
+					&cb_in.cb_completion, HZ / 2),
 			>, 0);
 		UT_ASSERT_INT(cb_in.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_in.event_entry_update, ==, 1);
@@ -550,7 +550,7 @@ static void smp2p_ut_remote_inout_core(struct seq_file *s, int remote_pid)
 		/* Verify inbound reply */
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_in.cb_completion, msecs_to_jiffies(500)),
+					&cb_in.cb_completion, HZ / 2),
 			>, 0);
 		UT_ASSERT_INT(cb_in.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_in.event_entry_update, ==, 1);
@@ -574,7 +574,7 @@ static void smp2p_ut_remote_inout_core(struct seq_file *s, int remote_pid)
 		/* Verify inbound reply */
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_in.cb_completion, msecs_to_jiffies(500)),
+					&cb_in.cb_completion, HZ / 2),
 			>, 0);
 		UT_ASSERT_INT(cb_in.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_in.event_entry_update, ==, 1);
@@ -599,7 +599,7 @@ static void smp2p_ut_remote_inout_core(struct seq_file *s, int remote_pid)
 
 		UT_ASSERT_INT(
 			(int)wait_for_completion_timeout(
-					&cb_in.cb_completion, msecs_to_jiffies(500)),
+					&cb_in.cb_completion, HZ / 2),
 			==, 0);
 		UT_ASSERT_INT(cb_in.cb_count, ==, 0);
 		UT_ASSERT_INT(cb_in.event_entry_update, ==, 0);
@@ -813,7 +813,7 @@ static void smp2p_ut_local_in_max_entries(struct seq_file *s)
 			UT_ASSERT_INT(ret, ==, 0);
 			UT_ASSERT_INT(
 				(int)wait_for_completion_timeout(
-					&(cb_in[j].cb_completion), msecs_to_jiffies(500)),
+					&(cb_in[j].cb_completion), HZ / 2),
 				>, 0);
 			UT_ASSERT_INT(cb_in[j].cb_count, ==, 1);
 			UT_ASSERT_INT(cb_in[j].event_entry_update, ==, 0);
@@ -908,7 +908,7 @@ static void smp2p_ut_local_in_multiple(struct seq_file *s)
 		UT_ASSERT_INT(ret, ==, 0);
 		UT_ASSERT_INT(
 				(int)wait_for_completion_timeout(
-				&(cb_in_1.cb_completion), msecs_to_jiffies(500)),
+				&(cb_in_1.cb_completion), HZ / 2),
 				>, 0);
 		UT_ASSERT_INT(cb_in_1.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_in_1.event_entry_update, ==, 0);
@@ -919,7 +919,7 @@ static void smp2p_ut_local_in_multiple(struct seq_file *s)
 		UT_ASSERT_INT(ret, ==, 0);
 		UT_ASSERT_INT(
 				(int)wait_for_completion_timeout(
-				&(cb_in_2.cb_completion), msecs_to_jiffies(500)),
+				&(cb_in_2.cb_completion), HZ / 2),
 				>, 0);
 		UT_ASSERT_INT(cb_in_2.cb_count, ==, 1);
 		UT_ASSERT_INT(cb_in_2.event_entry_update, ==, 0);
