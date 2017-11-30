928720a6a6c70c038d3bae6a029c08201eeba234
carck <carck@foxmail.com>
2017-11-30 13:46:22 +0800

    fix sepolicy warning

---
+++ mkbootimg_tools/fix-seplocify.sh
@@ -0,0 +1,46 @@
+
+./sepolicy-inject -s system_server -t rootfs -c system -p module_load -P sepolicy
+./sepolicy-inject -s system_server -t app_data_file -c file -p open,read,write -P sepolicy
+./sepolicy-inject -s system_server -t default_prop -c property_service -p set -P sepolicy
+./sepolicy-inject -s shell -t rootfs -c file -p getattr -P sepolicy
+./sepolicy-inject -s mt_daemon -t rootfs -c dir -p open,read -P sepolicy
+./sepolicy-inject -s mt_daemon -t rootfs -c lnk_file -p getattr -P sepolicy
+./sepolicy-inject -s mt_daemon -t device -c dir -p open,read -P sepolicy
+./sepolicy-inject -s mt_daemon -t block_device -c dir -p open,read,search -P sepolicy
+./sepolicy-inject -s mt_daemon -t storage_file -c dir -p getattr -P sepolicy
+./sepolicy-inject -s mt_daemon -t adsprpcd_file -c dir -p getattr -P sepolicy
+./sepolicy-inject -s mt_daemon -t cache_file -c dir -p getattr -P sepolicy
+./sepolicy-inject -s mt_daemon -t tmpfs -c dir -p search -P sepolicy
+
+./sepolicy-inject -s fsck -t block_device -c blk_file -p getattr,open,read,write,ioctl -P sepolicy
+./sepolicy-inject -s init -t debugfs_trace_marker -c file -p getattr -P sepolicy
+./sepolicy-inject -s rmt_storage -t debugfs -c file -p open,read,write -P sepolicy
+./sepolicy-inject -s miui_init_shell -t rootfs -c lnk_file -p getattr -P sepolicy
+./sepolicy-inject -s miui_init_shell -t rootfs -c dir -p open,read -P sepolicy
+./sepolicy-inject -s smcinvoke_daemon -t rootfs -c lnk_file -p getattr -P sepolicy
+./sepolicy-inject -s fdpp -t rootfs -c lnk_file -p getattr -P sepolicy
+./sepolicy-inject -s shelld -t rootfs -c lnk_file -p getattr -P sepolicy
+./sepolicy-inject -s vsim_daemon -t rootfs -c lnk_file -p getattr -P sepolicy
+./sepolicy-inject -s qvrd -t rootfs -c lnk_file -p getattr -P sepolicy
+./sepolicy-inject -s mcd -t rootfs -c lnk_file -p getattr -P sepolicy
+./sepolicy-inject -s mcd -t mcd -c capability -p sys_ptrace -P sepolicy
+
+./sepolicy-inject -s time_daemon -t diag_device -c chr_file -p open,read,write,ioctl -P sepolicy
+./sepolicy-inject -s dpmd -t diag_device -c chr_file -p open,read,write,ioctl -P sepolicy
+./sepolicy-inject -s sensors -t diag_device -c chr_file -p open,read,write,ioctl -P sepolicy
+./sepolicy-inject -s cnd -t diag_device -c chr_file -p open,read,write,ioctl -P sepolicy
+./sepolicy-inject -s ipacm-diag -t diag_device -c chr_file -p open,read,write,ioctl -P sepolicy
+./sepolicy-inject -s thermal-engine -t diag_device -c chr_file -p open,read,write,ioctl -P sepolicy
+./sepolicy-inject -s surfaceflinger -t diag_device -c chr_file -p open,read,write,ioctl -P sepolicy
+
+./sepolicy-inject -s nv_mac -t property_socket -c sock_file -p open,read,write -P sepolicy
+./sepolicy-inject -s nv_mac -t init -c unix_stream_socket -p read,write,connectto -P sepolicy
+./sepolicy-inject -s nv_mac -t default_prop -c property_service -p set -P sepolicy
+./sepolicy-inject -s radio -t net_radio_prop -cproperty_servicedir -p set -P sepolicy
+
+./sepolicy-inject -s zygote -t mnt_expand_file -c dir -p getattr -P sepolicy
+./sepolicy-inject -s qti_init_shell -t sysfs -c dir -p write -P sepolicy
+
+
+
+
