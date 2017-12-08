770700c1b5ac145c10ba4d54cc1f0cad3c3797c0
carck <carck@foxmail.com>
2017-12-08 16:54:12 +0800

    misc: try to speedup fingerprint resume

--- drivers/input/fingerprint/goodix_ta/gf_spi.c
+++ drivers/input/fingerprint/goodix_ta/gf_spi.c
@@ -670,7 +670,7 @@ static void goodix_suspend_resume(struct work_struct *work)
 	if (!data->fb_black)
 		set_fingerprintd_nice(0);
 	else
-		set_fingerprintd_nice(MIN_NICE);
+		set_fingerprintd_nice(-1);
 }
 
 static int goodix_fb_state_chg_callback(struct notifier_block *nb,
