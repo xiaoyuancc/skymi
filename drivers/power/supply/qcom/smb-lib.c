1ac7283a55e6ee1631d1eaadca253eebb5ae84de
carck <carck@foxmail.com>
2018-01-15 16:36:42 +0800

    smb-lib: Enable usb fast charge up to 900mA

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -999,6 +999,11 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 	int rc;
 	u8 icl_options;
 
+	if (icl_ua == USBIN_500MA){
+		icl_ua = USBIN_900MA;
+		pr_info("Trigger USB charge with 900mA\n");
+	}
+
 	/* power source is SDP */
 	switch (icl_ua) {
 	case USBIN_100MA:
